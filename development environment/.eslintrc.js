aa5e4191cb434b632d12bd33c4e9706fd4508844
정석우 <ingal777@naver.com>
2022-04-04 11:35:22 +0900

    prettier setup (eslint 파일에 prettier 도 같이 실행) -> 플러그인 2개 설치

--- development environment/.eslintrc.js
+++ development environment/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",//rules를 수동으로 입력x,recommended규칙대로
+    "extends": ["eslint:recommended","plugin:prettier/recommended"],//rules를 수동으로 입력x,recommended규칙대로 //eslint,prettier동시에 실행
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -13,8 +13,9 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-  //    "prettier/prettier": "error",
   //    "no-unexpected-multiline":"error", //함수간 따음표 처리
   //    "no-extra-semi":"error" // 여러개 따음표 에러처리
    }
 };
+
+
